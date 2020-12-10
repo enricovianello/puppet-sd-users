a839305bc3c1d10c61992357dde6814cd82d26ba
enricovianello <maverick17586@gmail.com>
2020-12-10 13:47:21 +0100

    v0.1.2
    Added fagostini into sd_users

--- spec/classes/sdds_users_spec.rb
+++ spec/classes/sdds_users_spec.rb
@@ -13,6 +13,7 @@ describe 'sdds_users' do
         is_expected.to contain_sdds_users__authorized_user('msoares')
         is_expected.to contain_sdds_users__authorized_user('ronchieri')
         is_expected.to contain_sdds_users__authorized_user('giaco')
+        is_expected.to contain_sdds_users__authorized_user('fagostini')
       end
 
       it 'check ds authorized users' do
